e65359909b5eba650336904ff4c6585a7895f23f
Dewadithya <Dewadithyapp@gmail.com>
2022-07-03 12:23:16 +0530

    FrontEnd Changes

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -2,12 +2,12 @@ import { Selector } from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
-    .page`http://localhost:8080/addStudent`
+    .page`http://localhost:8080/api/addTeacher`
 
 test('Testing add students', async t => {
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo("/api/dbinitialize");
 
-    await t.navigateTo("/addStudent");
+    await t.navigateTo("/api/addTeacher");
     await t.typeText("#student-id", "999999");
     await t.typeText("#student-name", "Pasindu Basnayaka");
     await t.typeText("#student-age", "45");
